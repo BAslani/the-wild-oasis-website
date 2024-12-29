d5cbd91b07ef9f93283fb2b465c4f79060834212
behzad aslani <behzadaslani80@gmail.com>
2024-12-29 18:49:18 +0330

    feat: implemented creating a new guest on signup

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthConfig } from 'next-auth'
 import Google from 'next-auth/providers/google'
+import { createGuest, getGuest } from './data-service'
 
 const authConfig: NextAuthConfig = {
   providers: [
@@ -11,6 +12,25 @@ const authConfig: NextAuthConfig = {
   callbacks: {
     authorized({ auth }) {
       return !!auth?.user
+    },
+    async signIn({ user }) {
+      try {
+        if (!user.email) return false
+        const existingGuest = await getGuest(user.email)
+        if (!existingGuest && user.name && user.email)
+          await createGuest({
+            fullName: user.name,
+            email: user.email,
+          })
+        return true
+      } catch {
+        return false
+      }
+    },
+    async session({ session }) {
+      const guest = await getGuest(session.user.email)
+      session.user.guestId = guest.id
+      return session
     },
   },
   pages: {
--- app/_lib/data-service.ts
+++ app/_lib/data-service.ts
@@ -152,7 +152,7 @@ export async function getCountries() {
 /////////////
 // CREATE
 
-export async function createGuest(newGuest: GuestType) {
+export async function createGuest(newGuest: Partial<GuestType>) {
   const { data, error } = await supabase.from('guests').insert([newGuest])
 
   if (error) {
